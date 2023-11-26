31d657a48a735c244c50efa72cb57e2ecf20082d
Pedro Henrique Segatti <pedrosegatti_20@hotmail.com>
2023-11-26 17:12:55 -0300

    Botão Scroll Top

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -1,21 +1,33 @@
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation, Route, Routes, Navigate } from "react-router-dom";
 
 import AdminNavbar from "components/Navbars/AdminNavbar.js";
 import Sidebar from "components/Sidebar/Sidebar.js";
-
+import { Button } from "reactstrap";
 import routes from "routes.js";
 
 const Admin = (props) => {
   const mainContent = React.useRef(null);
-  const location = useLocation();
+  const [showScrollButton, setShowScrollButton] = useState(false);
 
-  React.useEffect(() => {
+  useEffect(() => {
+    const handleScroll = () => {
+      const isTop = document.documentElement.scrollTop > 100;
+      setShowScrollButton(isTop);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
     document.documentElement.scrollTop = 0;
     document.scrollingElement.scrollTop = 0;
     mainContent.current.scrollTop = 0;
-  }, [location]);
+  };
 
   const getRoutes = (routes) => {
     return routes.map((prop, key) => {
@@ -44,6 +56,18 @@ const Admin = (props) => {
           <Route path="*" element={<Navigate to="/admin/index" replace />} />
         </Routes>
       </div>
+      {showScrollButton && (
+        <Button className="btn-icon btn-2 fa-bounce" color="success" onClick={scrollToTop} type="button" style={{
+          position: "fixed",
+          bottom: "20px",
+          right: "20px",
+          display: "block",
+        }}>
+          <span className="btn-inner-icon">
+            <i className="ni ni-bold-up fa-bounce" />
+          </span>
+        </Button>
+      )}
     </>
   );
 };
