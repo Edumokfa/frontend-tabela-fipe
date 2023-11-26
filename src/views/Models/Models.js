b05fcd96b1ee94bd16258f4fd1d0fa49152b0d2d
Eduardo Mokfa <82604894+Edumokfa@users.noreply.github.com>
2023-11-26 14:53:30 -0300

    Ajustes na tela de modelos/veiculos

--- src/views/Models/Models.js
+++ src/views/Models/Models.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { apiGet } from "../../middlewares/communicationMiddleware";
-import { Card, CardHeader, Table, Container, Row, CardBody } from "reactstrap";
-import { Toaster } from "react-hot-toast";
+import { apiGet, apiPut, apiPost, apiDelete, HttpStatus } from "../../middlewares/communicationMiddleware";
+import { Card, CardHeader, Table, Container, Row, CardBody, Button, Modal, Col, Form, FormGroup, Input } from "reactstrap";
+import toast, { Toaster } from "react-hot-toast";
 import Header from "components/Headers/Header.js";
 
 const AutoComplete = ({ options, onSelect }) => {
@@ -98,6 +98,10 @@ const Models = () => {
   const [modelList, setModelList] = useState([]);
   const [idToExpandRow, setIdToExpandRow] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [vehicleInEdit, setVehicleInEdit] = useState({"Codigo": {}});
+  const [open, setOpen] = useState(false);
+
+  const notify = (message) => toast(message);
 
   useEffect(() => {
     getBrands();
@@ -124,6 +128,42 @@ const Models = () => {
     setLoading(false);
   };
 
+  const saveVehicle = async (e) => {
+    e.preventDefault();
+
+    const SiglaCombustivel = vehicleInEdit.Combustivel === "Diesel" ? "D" : "G";
+
+    const vehiclePayLoad = {
+      "Codigo": vehicleInEdit.Codigo,
+      "AnoModelo": vehicleInEdit.AnoModelo,
+      "CodigoFipe": vehicleInEdit.CodigoFipe,
+      "Combustivel": vehicleInEdit.Combustivel,
+      "CodigoMarca": vehicleInEdit.CodigoMarca,
+      "Marca": vehicleInEdit.Marca,
+      "MesReferencia": vehicleInEdit.MesReferencia,
+      "Modelo": vehicleInEdit.Modelo,
+      "SiglaCombustivel": SiglaCombustivel,
+      "TipoVeiculo": "1",
+      "Valor": vehicleInEdit.Valor,
+    }
+
+    try {
+      var response;
+      if (vehicleInEdit.Codigo) {
+        response = await apiPut("/veiculos", vehiclePayLoad);
+      } else {
+        response = await apiPost("/veiculos", vehiclePayLoad);
+      }
+      if (response.status === HttpStatus.Ok) {
+        await getBrands();
+        handleClose();
+        notify("Registro Salvo com Sucesso");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   const handleIconClick = async (model) => {
     modelList.forEach((m) => {
       if (m.codigo !== model.codigo) {
@@ -143,6 +183,28 @@ const Models = () => {
       setIdToExpandRow(model.codigo);
     }
   };
+
+  const handleOpen = (vehicle) => {
+    setOpen(true);
+    setVehicleInEdit(vehicle.veiculo)
+  };
+  const handleClose = () => setOpen(false);
+
+  const deleteVehicle = async (e, vehicle, model) => {
+    e.preventDefault();
+    try {
+      const response = await apiDelete("/veiculos/" + vehicle.veiculo.Codigo.codigoFipe + "/" + vehicle.veiculo.Codigo.codigoAno);
+      if (response.status === HttpStatus.Ok) {
+        setModelList(modelList.map(modelo => {
+          modelo.anos = modelo.anos.filter(ano => ano.veiculo.Codigo.codigoFipe !== vehicle.veiculo.Codigo.codigoFipe || ano.veiculo.Codigo.codigoAno !== vehicle.veiculo.Codigo.codigoAno);
+          return modelo;
+        }))
+        notify("Registro Excluído com Sucesso");
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   const styleRotateIcon = {
     position: "relative",
@@ -165,15 +227,26 @@ const Models = () => {
                 <AutoComplete options={brandList} onSelect={handleSelect} />
               </CardBody>
 
-              {loading ? 
+              {loading ? (
                 <>
-                  <img src={require("../../assets/img/loading.gif")} alt="GIF carregando" style={{    
-                    width: "250px",
-                    alignSelf: "center"
-                  }} /> <p style={{    
-                    alignSelf: "center"
-                  }}>Buscando Informações </p> <br /> 
-                </> : 
+                  <img
+                    src={require("../../assets/img/loading.gif")}
+                    alt="GIF carregando"
+                    style={{
+                      width: "250px",
+                      alignSelf: "center",
+                    }}
+                  />{" "}
+                  <p
+                    style={{
+                      alignSelf: "center",
+                    }}
+                  >
+                    Buscando Informações{" "}
+                  </p>{" "}
+                  <br />
+                </>
+              ) : (
                 <Table className="align-items-center table-flush" responsive>
                   <thead className="thead-light">
                     {modelList.length !== 0 && (
@@ -229,25 +302,53 @@ const Models = () => {
                                       <th scope="col">Valor</th>
                                       <th scope="col">Combustível</th>
                                       <th scope="col">Ano / Modelo</th>
+                                      <th></th>
                                     </tr>
                                   </thead>
                                   <tbody>
-                                    {model.anos === 0
-                                      ? (
-                                        <tr key={`noYears_${model.codigo}`}>
-                                          <td colSpan="6">Sem Anos para exibir</td>
+                                    {model.anos === 0 ? (
+                                      <tr key={`noYears_${model.codigo}`}>
+                                        <td colSpan="6">
+                                          Sem Anos para exibir
+                                        </td>
+                                      </tr>
+                                    ) : (
+                                      model.anos.map((ano) => (
+                                        <tr key={`years_${ano.codigo}`}>
+                                          <td>{ano.codigo}</td>
+                                          <td>{ano.nome}</td>
+                                          <td>{ano.veiculo.Modelo}</td>
+                                          <td>{ano.veiculo.Valor}</td>
+                                          <td>{ano.veiculo.Combustivel}</td>
+                                          <td>{ano.veiculo.AnoModelo}</td>
+                                          <td className="text-right">
+                                            <Button
+                                              className="btn-icon btn-2"
+                                              color="primary"
+                                              type="button"
+                                              onClick={() => handleOpen(ano)}
+                                            >
+                                              <span className="btn-inner--icon">
+                                                <i className="ni ni-ruler-pencil" />
+                                              </span>
+                                            </Button>
+
+                                            <Button
+                                              className="btn-icon btn-2"
+                                              color="warning"
+                                              onClick={(e) =>
+                                                deleteVehicle(e, ano, model)
+                                              }
+                                              type="button"
+                                            >
+                                              <span className="btn-inner--icon">
+                                                <i className="ni ni-archive-2" />
+                                              </span>
+                                            </Button>
+                                          </td>
                                         </tr>
-                                      )
-                                      : model.anos.map((ano) => (
-                                          <tr key={`years_${ano.codigo}`}>
-                                            <td>{ano.codigo}</td>
-                                            <td>{ano.nome}</td>
-                                            <td>{ano.veiculo.Modelo}</td>
-                                            <td>{ano.veiculo.Valor}</td>
-                                            <td>{ano.veiculo.Combustivel}</td>
-                                            <td>{ano.veiculo.AnoModelo}</td>
-                                          </tr>
-                                        ))}
+                                      ))
+                                    )}
                                   </tbody>
                                 </Table>
                               </td>
@@ -258,11 +359,100 @@ const Models = () => {
                     )}
                   </tbody>
                 </Table>
-              }
+              )}
             </Card>
           </div>
         </Row>
       </Container>
+
+      <Modal
+        className="modal-dialog-centered"
+        isOpen={open}
+      >
+        <div className="modal-header">
+          <h5 className="modal-title" id="exampleModalLabel">
+            Veículos
+          </h5>
+          <button
+            aria-label="Close"
+            className="close"
+            data-dismiss="modal"
+            type="button"
+            onClick={() => handleClose()}
+          >
+            <span aria-hidden={true}>×</span>
+          </button>
+        </div>
+        <div className="modal-body">
+          <Form>
+            <Row>
+              <Col md="6">
+                <FormGroup>
+                  <p className="mb-0">Código</p>
+                  <Input disabled placeholder="Código" type="text" value={vehicleInEdit.Codigo.codigoAno} />
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col md="12">
+                <FormGroup>
+                  <p className="mb-0">Modelo</p>
+                  <Input placeholder="Nome" type="text" defaultValue={vehicleInEdit.Modelo} onChange={(e) => vehicleInEdit.Modelo = e.target.value} />
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col md="12">
+                <FormGroup>
+                  <p className="mb-0">Código Fipe</p>
+                  <Input placeholder="Nome" type="text" defaultValue={vehicleInEdit.CodigoFipe} onChange={(e) => vehicleInEdit.CodigoFipe = e.target.value} />
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col md="12">
+                <FormGroup>
+                  <p className="mb-0">Valor</p>
+                  <Input placeholder="Nome" type="text" defaultValue={vehicleInEdit.Valor} onChange={(e) => vehicleInEdit.Valor = e.target.value} />
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col md="12">
+                <FormGroup>
+                  <p className="mb-0">Ano / Modelo</p>
+                  <Input placeholder="Nome" type="number" defaultValue={vehicleInEdit.AnoModelo} onChange={(e) => vehicleInEdit.AnoModelo = e.target.value} />
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col md="12">
+                <FormGroup>
+                  <p className="mb-0">Combustível</p>
+                  <select className="form-control" defaultValue={vehicleInEdit.Combustivel} data-toggle="select" title="Tipo" data-placeholder="Selecione o combustível" onChange={(e) => vehicleInEdit.Combustivel = e.target.value} >
+                    <option>Gasolina</option>
+                    <option>Diesel</option>
+                  </select>
+                </FormGroup>
+              </Col>
+            </Row>
+          </Form>
+
+        </div>
+        <div className="modal-footer">
+          <Button
+            color="secondary"
+            data-dismiss="modal"
+            type="button"
+            onClick={() => handleClose()}
+          >
+            Fechar
+          </Button>
+          <Button color="success" type="button" onClick={(e) => saveVehicle(e)}>
+            Salvar
+          </Button>
+        </div>
+      </Modal>
     </>
   );
 };
