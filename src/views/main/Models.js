bd4b1961b4be34a98f1c6d75661056f73f0c3ac7
Eduardo Mokfa <82604894+Edumokfa@users.noreply.github.com>
2023-11-26 00:01:26 -0300

    Criado Gif carregando

--- src/views/main/Models.js
+++ src/views/main/Models.js
@@ -1,27 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { apiGet } from '../../middlewares/communicationMiddleware';
-import {
-  Card,
-  CardHeader,
-  Table,
-  Container,
-  Row,
-  CardBody
-} from "reactstrap";
-import { Toaster } from 'react-hot-toast';
+import React, { useEffect, useState } from "react";
+import { apiGet } from "../../middlewares/communicationMiddleware";
+import { Card, CardHeader, Table, Container, Row, CardBody } from "reactstrap";
+import { Toaster } from "react-hot-toast";
 import Header from "components/Headers/Header.js";
 
 const AutoComplete = ({ options, onSelect }) => {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [filteredOptions, setFilteredOptions] = useState([]);
 
   const handleInputChange = (e) => {
     const inputValue = e.target.value;
     setInputValue(inputValue);
 
-    const filteredOptions = options.filter(option =>
-      (option.codigo + option.tipo.toLowerCase() + option.nome.toLowerCase()).includes(inputValue.toLowerCase())
-    ).slice(0, 10);
+    const filteredOptions = options
+      .filter((option) =>
+        (
+          option.codigo +
+          option.tipo.toLowerCase() +
+          option.nome.toLowerCase()
+        ).includes(inputValue.toLowerCase())
+      )
+      .slice(0, 10);
 
     setFilteredOptions(filteredOptions);
   };
@@ -33,34 +32,34 @@ const AutoComplete = ({ options, onSelect }) => {
   };
 
   return (
-    <div style={{ position: 'relative', display: 'inline-block' }}>
+    <div style={{ position: "relative", display: "inline-block" }}>
       <input
         type="text"
         value={inputValue}
         onChange={handleInputChange}
         placeholder="Digite para buscar"
         style={{
-          padding: '8px',
-          fontSize: '16px',
-          width: '50vh',
-          borderRadius: '4px',
-          border: '1px solid #ccc',
+          padding: "8px",
+          fontSize: "16px",
+          width: "50vh",
+          borderRadius: "4px",
+          border: "1px solid #ccc",
         }}
       />
       {filteredOptions.length > 0 && (
         <ul
           style={{
-            listStyle: 'none',
+            listStyle: "none",
             padding: 0,
             margin: 0,
-            position: 'absolute',
-            top: '100%',
+            position: "absolute",
+            top: "100%",
             left: 0,
-            width: '50vh',
-            borderRadius: '4px',
-            border: '1px solid #ccc',
-            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-            backgroundColor: 'white',
+            width: "50vh",
+            borderRadius: "4px",
+            border: "1px solid #ccc",
+            boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+            backgroundColor: "white",
           }}
         >
           {filteredOptions.map((option, index) => (
@@ -68,18 +67,21 @@ const AutoComplete = ({ options, onSelect }) => {
               key={index}
               onClick={() => handleSelectOption(option)}
               style={{
-                padding: '8px',
-                cursor: 'pointer',
-                "z-index": "10",
-                position: 'relative',
-                "background-color": "white",
-                borderBottom: index < filteredOptions.length - 1 ? '1px solid #ccc' : 'none',
+                padding: "8px",
+                cursor: "pointer",
+                zIndex: "10",
+                position: "relative",
+                backgroundColor: "white",
+                borderBottom:
+                  index < filteredOptions.length - 1
+                    ? "1px solid #ccc"
+                    : "none",
               }}
               onMouseOver={(e) => {
-                e.target.style.backgroundColor = '#f0f0f0';
+                e.target.style.backgroundColor = "#f0f0f0";
               }}
               onMouseOut={(e) => {
-                e.target.style.backgroundColor = 'white';
+                e.target.style.backgroundColor = "white";
               }}
             >
               {option.nome + " | " + option.tipo}
@@ -95,6 +97,7 @@ const Models = () => {
   const [brandList, setBrandList] = useState([]);
   const [modelList, setModelList] = useState([]);
   const [idToExpandRow, setIdToExpandRow] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getBrands();
@@ -107,14 +110,23 @@ const Models = () => {
 
   const handleSelect = async (selectedOption) => {
     const codigo = selectedOption.codigo;
-    const response = await apiGet("/marcas/" + codigo + "/modelos");
-    const responseModelList = response.data.map(model => ({ ...model, iconRotation: 0 }));
-    setModelList(responseModelList);
+    setLoading(true);
+    try{
+      const response = await apiGet("/marcas/" + codigo + "/modelos");
+      const responseModelList = response.data.map((model) => ({
+        ...model,
+        iconRotation: 0,
+      }));
+      setModelList(responseModelList);
+    } catch(exception){
+
+    }
+    setLoading(false);
   };
 
   const handleIconClick = async (model) => {
-    modelList.forEach(m => {
-      if ( m.codigo !== model.codigo ) {
+    modelList.forEach((m) => {
+      if (m.codigo !== model.codigo) {
         document.getElementById(m.codigo).style.transform = `rotate(0deg)`;
         m.iconRotation = 0;
       }
@@ -130,11 +142,11 @@ const Models = () => {
     } else {
       setIdToExpandRow(model.codigo);
     }
-  }
+  };
 
   const styleRotateIcon = {
-    "position": "relative"
-  }
+    position: "relative",
+  };
 
   return (
     <>
@@ -147,75 +159,97 @@ const Models = () => {
               <CardHeader className="border-0">
                 <h3 className="mb-0">Modelos</h3>
               </CardHeader>
-              
+
               <CardBody>
                 <p className="mb-0">Selecione uma marca</p>
                 <AutoComplete options={brandList} onSelect={handleSelect} />
               </CardBody>
 
-              <Table className="align-items-center table-flush" responsive>
+              {loading ? 
+                <>
+                  <img src={require("../../assets/img/loading.gif")} alt="GIF carregando" style={{    
+                    width: "250px",
+                    alignSelf: "center"
+                  }} /> <p style={{    
+                    alignSelf: "center"
+                  }}>Buscando Informações </p> <br /> 
+                </> : 
+                <Table className="align-items-center table-flush" responsive>
                   <thead className="thead-light">
-                  {modelList.length !== 0 &&
-                    <>
-                      <tr>
-                        <th scope="col" width="20" />
-                        <th scope="col" width="50">Código</th>
-                        <th scope="col">Descrição</th>
-                      </tr>
-                    </>
-                  }
+                    {modelList.length !== 0 && (
+                      <>
+                        <tr>
+                          <th scope="col" width="20" />
+                          <th scope="col" width="50">
+                            Código
+                          </th>
+                          <th scope="col">Descrição</th>
+                        </tr>
+                      </>
+                    )}
                   </thead>
                   <tbody>
-                    {modelList.length === 0 ? "Sem modelos para exibir" : modelList.map((model, index) => (
-                      <>
-                        <tr key={index}>
-                          <td className="text-left">
-                            <div id={model.codigo} style={styleRotateIcon}>
-                              <span
-                                className="btn-inner--icon"
-                                onClick={() => handleIconClick(model)}
-                              >
-                                <i className="ni ni-bold-right" />
-                              </span>
-                            </div>
-                          </td>
-                          <td>{model.codigo}</td>
-                          <td>{model.nome}</td>
-                        </tr>
-                        {idToExpandRow === model.codigo &&
-                          <tr key={index * -1}>
-                            <td colSpan="3">
-                            <Table style={{"width": "100%", "box-shadow": "0 4px 8px rgba(0, 0, 0, 0.1)"}}>
-                              <thead className="thead-light">
-                                <tr>
-                                  <th scope="col">Código</th>
-                                  <th scope="col">Descrição</th>
-                                  <th scope="col">Modelo</th>
-                                  <th scope="col">Valor</th>
-                                  <th scope="col">Combustível</th>
-                                  <th scope="col">Ano / Modelo</th>
-                                </tr>
-                              </thead>
-                              <tbody>
-                                {model.anos === 0 ? "Sem Anos para exibir" : model.anos.map((ano, index) => (
-                                  <tr key={index}>
-                                    <td>{ano.codigo}</td>
-                                    <td>{ano.nome}</td>
-                                    <td>{ano.veiculo.Modelo}</td>
-                                    <td>{ano.veiculo.Valor}</td>
-                                    <td>{ano.veiculo.Combustivel}</td>
-                                    <td>{ano.veiculo.AnoModelo}</td>
-                                  </tr>
-                                ))}
-                              </tbody>
-                            </Table>
-                            </td>
-                          </tr>
-                        }
-                      </>
-                    ))}
+                    {modelList.length === 0
+                      ? <p className="p-4"> Sem modelos para exibir </p>
+                      : modelList.map((model, index) => (
+                          <>
+                            <tr key={index}>
+                              <td className="text-left">
+                                <div id={model.codigo} style={styleRotateIcon}>
+                                  <span
+                                    className="btn-inner--icon"
+                                    onClick={() => handleIconClick(model)}
+                                  >
+                                    <i className="ni ni-bold-right" />
+                                  </span>
+                                </div>
+                              </td>
+                              <td>{model.codigo}</td>
+                              <td>{model.nome}</td>
+                            </tr>
+                            {idToExpandRow === model.codigo && (
+                              <tr key={index * -1}>
+                                <td colSpan="3">
+                                  <Table
+                                    style={{
+                                      width: "100%",
+                                      "box-shadow":
+                                        "0 4px 8px rgba(0, 0, 0, 0.1)",
+                                    }}
+                                  >
+                                    <thead className="thead-light">
+                                      <tr>
+                                        <th scope="col">Código</th>
+                                        <th scope="col">Descrição</th>
+                                        <th scope="col">Modelo</th>
+                                        <th scope="col">Valor</th>
+                                        <th scope="col">Combustível</th>
+                                        <th scope="col">Ano / Modelo</th>
+                                      </tr>
+                                    </thead>
+                                    <tbody>
+                                      {model.anos === 0
+                                        ? "Sem Anos para exibir"
+                                        : model.anos.map((ano, index) => (
+                                            <tr key={index}>
+                                              <td>{ano.codigo}</td>
+                                              <td>{ano.nome}</td>
+                                              <td>{ano.veiculo.Modelo}</td>
+                                              <td>{ano.veiculo.Valor}</td>
+                                              <td>{ano.veiculo.Combustivel}</td>
+                                              <td>{ano.veiculo.AnoModelo}</td>
+                                            </tr>
+                                          ))}
+                                    </tbody>
+                                  </Table>
+                                </td>
+                              </tr>
+                            )}
+                          </>
+                        ))}
                   </tbody>
-              </Table>
+                </Table>
+              }
             </Card>
           </div>
         </Row>
